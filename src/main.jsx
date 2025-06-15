fa6d66e93a778b4a161d4986d81b560ae1804888
Mohamed Nooh <noohmohamed1999@gmail.com>
2025-06-15 21:54:02 +0300

    laste change remove basename from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/blog">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
